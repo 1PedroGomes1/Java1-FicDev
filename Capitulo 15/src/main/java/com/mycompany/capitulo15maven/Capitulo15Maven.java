893cada3a3e8490820e2c91c59e685e975249a34
Pedro H. S. Gomes <131673240+1PedroGomes1@users.noreply.github.com>
2023-06-21 23:27:05 -0400

    Submissão Dia 15 - 12/06/2023

---
+++ Capitulo 15/src/main/java/com/mycompany/capitulo15maven/Capitulo15Maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.capitulo15maven;
+
+/**
+ *
+ * @author ficdev
+ */
+public class Capitulo15Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
